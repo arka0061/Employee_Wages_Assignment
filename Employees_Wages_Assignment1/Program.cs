ef22131fbf6704726b05f667cf807e1aa89a3946
Arka <arka.spectacular@gmail.com>
2021-07-21 10:22:09 +0530

    [Arka]Add:Adding a welcome message

---
+++ Employees_Wages_Assignment1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employees_Wages_Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program!");
+        }
+    }
+}
