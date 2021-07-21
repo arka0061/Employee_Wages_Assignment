a0ebf17ed0e2406e5f27ea4a711e57c6d423b829
Arka <arka.spectacular@gmail.com>
2021-07-21 10:28:22 +0530

    [Arka]Add:Checked if Employee is absent or present

--- Employees_Wages_Assignment1/Program.cs
+++ Employees_Wages_Assignment1/Program.cs
@@ -6,7 +6,17 @@ namespace Employees_Wages_Assignment1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program!");
+            Random random = new Random();
+            int emp_Chceck = random.Next(0, 2);
+            if(emp_Chceck==1)
+            {
+                Console.WriteLine("Employee is present!");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent!");
+            }
+            
         }
     }
 }
