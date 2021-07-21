275f6c7a80bdb2250f6f77879735dd193b7ca9df
Arka <arka.spectacular@gmail.com>
2021-07-21 10:50:32 +0530

    [Arka]Add:Added employee wage per hour

--- Employees_Wages_Assignment1/Program.cs
+++ Employees_Wages_Assignment1/Program.cs
@@ -6,16 +6,22 @@ namespace Employees_Wages_Assignment1
     {
         static void Main(string[] args)
         {
+            int emp_rate_per_hr = 20;
             Random random = new Random();
             int emp_Chceck = random.Next(0, 2);
+            int emp_wage = 0;
+            int emp_hrs = 0;
+
             if(emp_Chceck==1)
             {
-                Console.WriteLine("Employee is present!");
+                emp_hrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent!");
+                emp_hrs = 4 ;
             }
+            emp_wage = emp_hrs * emp_rate_per_hr;
+            Console.WriteLine("The Wage of Employee is :" + emp_wage);
             
         }
     }
