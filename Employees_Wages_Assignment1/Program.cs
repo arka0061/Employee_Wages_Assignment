ca6a58967381c913fb9fbac6ada22be76761b288
Arka <arka.spectacular@gmail.com>
2021-07-24 13:13:44 +0530

    [Arka]Refactor:Fixed issues with the code

--- Employees_Wages_Assignment1/Program.cs
+++ Employees_Wages_Assignment1/Program.cs
@@ -8,38 +8,38 @@ namespace Employees_Wages_Assignment1
         {
             int emp_rate_per_hr = 20;
             int days = 0;
+            int emp_wage = 0;
+            int emp_hrs = 0;
             int working_hrs = 0;
             while (days <= 20 || working_hrs <= 100)
             {
 
                 Random random = new Random();
                 int emp_Chceck = random.Next(0, 2);
-                int emp_wage = 0;
-                int emp_hrs = 0;
-              
-                  switch (emp_Chceck)
-                  {
-                      case 0:
-                          Console.WriteLine("Employee is Absent");
-                          emp_hrs = 4;
-                          break;
-                      case 1:
-                          Console.WriteLine("Employee is Absent");
-                          emp_hrs = 8;
-                          break;
-                      default:
-                          Console.WriteLine("Error");                         
-                          break;
-                  }
-                days++;
-                working_hrs++;
 
-              }
-              emp_wage = emp_hrs * emp_rate_per_hr;
-             
-             Console.WriteLine("Employer Wage is : " + emp_wage);
-                             
-             }
+                switch (emp_Chceck)
+                {
+                    case 0:
+                        Console.WriteLine("Employee is Absent");
+                        emp_hrs = 4;
+                        break;
+                    case 1:
+                        Console.WriteLine("Employee is Present");
+                        emp_hrs = 8;
+                        break;
+                    default:
+                        Console.WriteLine("Error");
+                        break;
+                }
+                days++;
+                working_hrs = working_hrs + emp_hrs;
+
             }
+            emp_wage = working_hrs * emp_rate_per_hr;
+
+            Console.WriteLine("Employer Wage is : " + emp_wage);
+
         }
+    }
 }
+
