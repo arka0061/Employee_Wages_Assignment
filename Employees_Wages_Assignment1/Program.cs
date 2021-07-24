7fbf72a395c70d83ed6920797538ba71f8b449f8
arka0061 <38386553+arka0061@users.noreply.github.com>
2021-07-24 13:22:11 +0530

    Fixed

--- Employees_Wages_Assignment1/Program.cs
+++ Employees_Wages_Assignment1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Employees_Wages_Assignment1
 {
@@ -8,50 +8,38 @@ namespace Employees_Wages_Assignment1
         {
             int emp_rate_per_hr = 20;
             int days = 0;
+            int emp_wage = 0;
+            int emp_hrs = 0;
             int working_hrs = 0;
             while (days <= 20 || working_hrs <= 100)
             {
 
                 Random random = new Random();
                 int emp_Chceck = random.Next(0, 2);
-                int emp_wage = 0;
-                int emp_hrs = 0;
-                if (emp_Chceck == 1)
+
+                switch (emp_Chceck)
                 {
-                    Console.WriteLine("Employee is present!");
-                    emp_hrs = 8;
+                    case 0:
+                        Console.WriteLine("Employee is Absent");
+                        emp_hrs = 4;
+                        break;
+                    case 1:
+                        Console.WriteLine("Employee is Present");
+                        emp_hrs = 8;
+                        break;
+                    default:
+                        Console.WriteLine("Error");
+                        break;
                 }
-                else
-                {
-                    Console.WriteLine("Employee is Absent!");
-                    emp_hrs = 4;
-                }
-                emp_wage = emp_hrs * emp_rate_per_hr;
-                Console.WriteLine("The Wage of Employee is :" + emp_wage);
-
-
-                Console.WriteLine("Days :" + days + "Working hours"+ working_hrs);
-                /*  switch (emp_Chceck)
-                  {
-                      case 0:
-                          emp_hrs = 4;
-                          break;
-                      case 1:
-                          emp_hrs = 8;
-                          break;
-                      default:
-                          emp_hrs = 0;
-                          break;
-                  }
-
-              }
-              emp_wage = emp_hrs * emp_rate_per_hr;
-             
-             Console.WriteLine("Employer Wage is : " + emp_wage);
-                 */
                 days++;
-                working_hrs++;
-             }
+                working_hrs = working_hrs + emp_hrs;
+
             }
+            emp_wage = working_hrs * emp_rate_per_hr;
+
+            Console.WriteLine("Employer Wage is : " + emp_wage);
+
         }
+    }
 }
+
