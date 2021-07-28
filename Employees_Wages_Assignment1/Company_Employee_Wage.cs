c4cba258f02a2a0a0180e8caccbb57cb1f8c67a0
Arka <arka.spectacular@gmail.com>
2021-07-29 00:18:26 +0530

    [Arka]Add:Added list for companies

---
+++ Employees_Wages_Assignment1/Company_Employee_Wage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Employees_Wages_Assignment1
+{
+	public class Company_Employee_Wage
+	{
+		public String Company_Name { get; set; }
+		public int Wage_Per_Hour{ get; set; }
+		public int Max_Working_Days { get; set; }
+		public int Max_Working_Hours { get; set; }
+		public int Total_Wage = 0;
+	}
+}
+	
+
+
+
+
+
+
--- Employees_Wages_Assignment1/Program.cs
+++ Employees_Wages_Assignment1/Program.cs
@@ -6,20 +6,22 @@ namespace Employees_Wages_Assignment1
 {
     class Program
     {
-        
+        public static List<Company_Employee_Wage> Company = new List<Company_Employee_Wage>();
         public static void Main(string[] args)
         {
-            EmpWageBuilder obj1 = new EmpWageBuilder("Google",5, 27, 120);
-            EmpWageBuilder obj2 = new EmpWageBuilder("Google", 6, 30, 110);
-            ///Company 1 object
-            obj1.Employee_Wage(); 
-            obj1.Display();
-            ///Company 2 object
-            obj2.Employee_Wage();
-            obj2.Display();
+            EmpWageBuilder emp = new EmpWageBuilder();
+            Company.Add(new Company_Employee_Wage()
+            { Company_Name = "Google", Wage_Per_Hour = 6, Max_Working_Days = 29, Max_Working_Hours = 130 });
+            Company.Add(new Company_Employee_Wage()
+            { Company_Name = "Amazon", Wage_Per_Hour = 7, Max_Working_Days = 30, Max_Working_Hours = 120 });
+            Company.Add(new Company_Employee_Wage()
+            { Company_Name = "Google", Wage_Per_Hour = 9, Max_Working_Days = 25, Max_Working_Hours = 110 });
+            emp.Wage_Computation();
+            }
         }
-       
     }
-}
+
+
+
 
 
