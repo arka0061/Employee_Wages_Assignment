b36c974ad4a5d9f98dc61442930148428b44e209
Arka <arka.spectacular@gmail.com>
2021-07-29 12:44:35 +0530

    [Arka]Add:Added EmpWageBuilder to manage companies

---
+++ Employees_Wages_Assignment1/IEmpWageBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employees_Wages_Assignment1
+{
+    public interface IEmpWageBuilder
+    {
+        public void Wage_Computation();
+        public void Display(CompanyEmployeWage cmp);
+    }
+}
